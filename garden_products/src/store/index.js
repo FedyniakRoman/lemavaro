fb3cb6a7532364c429c4ea7ddc42f96d4b02a82e
Beljawski <155641687+Beljawski@users.noreply.github.com>
2024-10-01 22:23:47 +0200

    Project_creation_and_first_commit

---
+++ garden_products/src/store/index.js
@@ -0,0 +1,5 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { thunk } from 'redux-thunk'
+
+const rootReducer = combineReducers({})
+export const store = createStore(rootReducer, applyMiddleware(thunk));
