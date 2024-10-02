bc41b7c63a01aabc09925d07f1fef3d78d2ee26f
Beljawski <155641687+Beljawski@users.noreply.github.com>
2024-10-02 13:47:01 +0200

    KAN-13_setting_routing_and_creation_of_blank_pages

--- garden_products/src/App.js
+++ garden_products/src/App.js
@@ -1,9 +1,29 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import MainPage from './pages/MainPage';
+import ProductsPage from './pages/ProductsPage';
+import SingleProductPage from './pages/SingleProductPage';
+import CategoriesPage from './pages/CategoriesPage';
+import ProductsByCategoryPage from './pages/ProductsByCategoryPage';
+import NotFoundPage from './pages/NotFoundPage';
+import CartPage from './pages/CartPage';
+import SalesPage from './pages/SalesPage';
+import FavoritesPage from './pages/FavoritesPage';
 
 function App() {
   return (
     <div className="App">
-      123
+     <Routes>
+     <Route path='/' element={<MainPage/>} />
+        <Route path='/products' element={<ProductsPage/>} />
+        <Route path='/products/:id' element={<SingleProductPage/>} />
+        <Route path='/cart' element={<CartPage />} />
+        <Route path='/categories' element={<CategoriesPage/>} />
+        <Route path='/categories/:category_name' element={<ProductsByCategoryPage/>} />
+        <Route path='/sales' element=<SalesPage/> />
+        <Route path='/favorites' element={<FavoritesPage/>} />
+        <Route path='*' element={<NotFoundPage/>} />
+     </Routes>
     </div>
   );
 }
