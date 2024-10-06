bcc8800bb4e35b4bee3bf6996cfa057c488066bd
Beljawski <155641687+Beljawski@users.noreply.github.com>
2024-10-06 19:33:13 +0200

    home-page-sales-products-ready

---
+++ garden_products/src/requests/products.js
@@ -0,0 +1,9 @@
+import { loadProductsAction } from "../store/reducers/productsReducer";
+
+export const getAllProducts =(dispatch)=>{
+    fetch('http://localhost:3333/products/all')
+    .then(res => res.json())
+    .then(json=> dispatch(loadProductsAction(json)))
+    .catch(err => console.error('Error fetching products:', err));
+}
+
