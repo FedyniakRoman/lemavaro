6a0f18cc4f3d60119b6382d0dcae764551145249
Beljawski <155641687+Beljawski@users.noreply.github.com>
2024-10-11 17:50:51 +0200

    save_after_merge

--- garden_products/src/pages/CartPage/index.jsx
+++ garden_products/src/pages/CartPage/index.jsx
@@ -1,24 +1,36 @@
-import React, { useEffect } from 'react'
-import CartCortainer from '../../components/CartCortainer'
-import { useSelector } from 'react-redux'
-import s from './index.module.css'
+import React, { useEffect } from "react";
+import CartCortainer from "../../components/CartCortainer";
+import { useSelector } from "react-redux";
+import s from "./index.module.css";
+import { Link } from "react-router-dom";
 
 function CartPage() {
-
-  const cartState = useSelector(store => store.cart)
+  const cartState = useSelector((store) => store.cart);
 
   return (
     <section className={s.container}>
-       <div className={s.wrapper}>
-      <h2 className={s.title}>Shopping cart</h2>
-    <div className={s.cart_container}>
-      {cartState.length === 0 
-      ? <p>Looks like you have no items in your basket currently.</p>
-      : <CartCortainer products={cartState}/>}
+      <div className={s.title_container}>
+        <h2 className={s.title}>Shopping cart</h2>
+        <div className={s.nav_container}>
+        <div className={s.nav_list}>
+          <div className={s.linie}></div>
+          <Link to={"/"} className={s.item}>
+            Back to the store
+          </Link>
+          </div>
+        </div>
       </div>
+      <div className={s.wrapper}>
+        <div className={s.cart_container}>
+          {cartState.length === 0 ? (
+            <p>Looks like you have no items in your basket currently.</p>
+          ) : (
+            <CartCortainer products={cartState} />
+          )}
+        </div>
       </div>
     </section>
-  )
+  );
 }
 
-export default CartPage
+export default CartPage;
