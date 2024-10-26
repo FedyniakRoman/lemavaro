8ca52784802f05bc4a723170ac6e95c407949a42
Beljawski <155641687+Beljawski@users.noreply.github.com>
2024-10-26 17:37:38 +0200

    update_push

--- garden_products/src/store/reducers/productsReducer.js
+++ garden_products/src/store/reducers/productsReducer.js
@@ -1,13 +1,13 @@
 const defaultProductsState = {
   products: [], // Изначальный массив продуктов
-  status: "loading", // Статус загрузки
+  status: 'loading', // Статус загрузки
 };
 
-const LOAD_PRODUCTS = "LOAD_PRODUCTS";
-const CHANGE_STATUS_TO_LOADING = "CHANGE_STATUS_TO_LOADING";
-const SORT_ALL_PRODUCTS = "SORT_ALL_PRODUCTS";
-const GET_DISCOUNT_PRODUCTS = "GET_DISCOUNT_PRODUCTS";
-const FILTER_BY_PRICE = "FILTER_BY_PRICE";
+const LOAD_PRODUCTS = 'LOAD_PRODUCTS';
+const CHANGE_STATUS_TO_LOADING = 'CHANGE_STATUS_TO_LOADING';
+const SORT_ALL_PRODUCTS = 'SORT_ALL_PRODUCTS';
+const GET_DISCOUNT_PRODUCTS = 'GET_DISCOUNT_PRODUCTS';
+const FILTER_BY_PRICE = 'FILTER_BY_PRICE';
 
 export const loadProductsAction = (products) => ({
   type: LOAD_PRODUCTS,
@@ -38,24 +38,24 @@ export const productsReducer = (state = defaultProductsState, action) => {
     console.log("Action Payload productsReducer:", action.payload);
     return {
       products: action.payload.map((el) => ({ ...el, visible: true })), // Загружаем новые продукты
-      status: "ready", // Статус готовности
+      status: 'ready', // Статус готовности
     };
   } else if (action.type === CHANGE_STATUS_TO_LOADING) {
     return {
       ...state,
-      status: "loading", // Устанавливаем статус загрузки
+      status: 'loading', // Устанавливаем статус загрузки
     };
   } else if (action.type === SORT_ALL_PRODUCTS) {
     const sortedProducts = [...state.products]; // Копируем продукты для сортировки
 
     // Проверяем опцию сортировки
-    if (action.payload === "price_asc") {
+    if (action.payload === 'price_asc') {
       sortedProducts.sort((a, b) => a.price - b.price); // Сортировка по возрастанию цены
-    } else if (action.payload === "price_desc") {
+    } else if (action.payload === 'price_desc') {
       sortedProducts.sort((a, b) => b.price - a.price); // Сортировка по убыванию цены
-    } else if (action.payload === "nameAz") {
+    } else if (action.payload === 'nameAz') {
       sortedProducts.sort((a, b) => a.title.localeCompare(b.title)); // Сортировка по алфавиту (A-Z)
-    } else if (action.payload === "nameZa") {
+    } else if (action.payload === 'nameZa') {
       sortedProducts.sort((a, b) => b.title.localeCompare(a.title)); // Сортировка по алфавиту (Z-A)
     }
     // Если опция "default", возвращаем исходные продукты без изменений
