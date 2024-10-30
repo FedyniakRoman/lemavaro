b374024f027ed947523a774eca41521f509ec796
Beljawski <155641687+Beljawski@users.noreply.github.com>
2024-10-30 11:05:20 +0100

    validation_added

--- garden_products/src/components/OrderForm/index.jsx
+++ garden_products/src/components/OrderForm/index.jsx
@@ -1,48 +1,88 @@
-
 import { useSelector } from "react-redux";
+import { useForm } from "react-hook-form";
 import s from "./index.module.css";
 
-function OrderForm({ addNewOrder, setIsModalOpen }) {  
-    const cartState = useSelector((store) => store.cart);
+function OrderForm({ addNewOrder, setIsModalOpen }) {
+  const cartState = useSelector((store) => store.cart);
 
-    // Рассчитываем общую сумму заказа
-    let totalSum = cartState.reduce((acc, elem) => {
-        return elem.discont_price !== null
-            ? acc + (elem.discont_price * elem.count)
-            : acc + (elem.price * elem.count);
-    }, 0);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+  // Рассчитываем общую сумму заказа
+  let totalSum = cartState.reduce((acc, elem) => {
+    return elem.discont_price !== null
+      ? acc + elem.discont_price * elem.count
+      : acc + elem.price * elem.count;
+  }, 0);
 
-    // Обработчик отправки формы
-    const submitOrder = (event) => {
-        event.preventDefault(); // Предотвращаем перезагрузку страницы при отправке формы
-        const { name, phone, email } = event.target; // Получаем данные из формы
-        const newOrder = {
-            id: Date.now(), // Создаем уникальный ID заказа
-            name: name.value,
-            phone: phone.value,
-            email: email.value
-        };
-        addNewOrder(newOrder); // Отправляем заказ
-        setIsModalOpen(true);  // Открываем модальное окно для подтверждения
-        event.target.reset(); // Очищаем форму
-    };
+  const order = (data) => {
+    console.log({
+      ...data,
+      cart: cartState,
+      sum: totalSum.toFixed(2),
+    });
+    addNewOrder(data); // Отправляем заказ
+    setIsModalOpen(true); // Открываем модальное окно для подтверждения
+    reset();
+  };
+  const registerName = register('name', {
+    required: '*The field "Name" is required'
+  });
 
-    return (
-        <div className={s.order_container}>
-            <h3 className={s.order_title}>Order details</h3>
-            <p className={s.total_items}>{`${cartState.length} items`}</p>
-            <div className={s.total_price_box}>
-                <p className={s.total_title}>Total</p>
-                <p className={s.total_sum}>{`$${totalSum.toFixed(2)}`}</p>
-            </div>
-            <form onSubmit={submitOrder} className={s.order_form}>
-                <input type="text" placeholder="Name" name="name" className={s.input} />
-                <input type="tel" placeholder="Phone number" name="phone" className={s.input} />
-                <input type="email" placeholder="Email" name="email" className={s.input} />
-                <button className={s.form_button}>Checkout</button>
-            </form>
-        </div>
-    );
+  const registerPhone = register('phone', {
+    required: '*The field "Phone" is required',
+    pattern: {
+      value: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/,
+      message: 'You entered the wrong phone.'
+    }
+  });
+
+  const registerEmail = register('email', {
+    required: '*The field "Email" is required',
+    pattern: {
+      value: /^\S+@\S+\.\S+$/,
+      message: 'You entered the wrong e-mail.'
+    }
+  });
+
+  return (
+    <div className={s.order_container}>
+      <h3 className={s.order_title}>Order details</h3>
+      <p className={s.total_items}>{`${cartState.length} items`}</p>
+      <div className={s.total_price_box}>
+        <p className={s.total_title}>Total</p>
+        <p className={s.total_sum}>{`$${totalSum.toFixed(2)}`}</p>
+      </div>
+      <form className={s.order_form} onSubmit={handleSubmit(order)}>
+        <input type="text" placeholder="Name" className={s.input} {...registerName} />
+        {
+          errors.name && <p>{errors.name?.message}</p>
+        }
+        <input
+          type="tel"
+          placeholder="Phone number"
+          className={s.input}
+          {...registerPhone}
+        />
+        {
+          errors.phone && <p>{errors.phone?.message}</p>
+        }
+        <input
+          type="email"
+          placeholder="Email"
+          className={s.input}
+          {...registerEmail} 
+        />
+            {
+          errors.email && <p>{errors.email?.message}</p>
+        }
+        <button className={s.form_button}>Checkout</button>
+      </form>
+    </div>
+  );
 }
 
 export default OrderForm;
